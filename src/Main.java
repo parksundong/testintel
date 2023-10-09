cc766a2535ca63e7e8573b1fbdfc93d2b8121b52
parksundong <king5901@naver.com>
2023-10-09 20:40:00 +0900

    first commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args, String id) {
+
+        User user = new User("id1", "park", 35);
+        int age = user.getAge();
+
+        System.out.println(age);
+    }
+    public void setUser(String id, String name, int age) {
+
+    }
+
+
+    
+}
